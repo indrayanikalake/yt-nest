f673a318c398b3f23fefcf8dd3fd985740d61f4f
Indrayani <indrayanikalake13@gmail.com>
2024-04-26 14:28:15 +0530

    sending response | setting HTTP Headers and status codes

--- src/albums.controller.ts
+++ src/albums.controller.ts
@@ -1,11 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Redirect,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('/users')
 export class AlbumsController {
-  @Get('/albums')
-  getAlbums() {
-    return {
-      message: 'albums',
-    };
+  @Post('/albums')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Header('cache-control', 'none')
+  @Header('X-Name', 'Albums')
+  @Redirect('/users/account', 302)
+  getAlbums(@Res({ passthrough: true }) res: Response) {
+    const rn = ~~(Math.random() * 10 * 1);
+    if (rn < 5) {
+      return {
+        url: '/users/account',
+        statusCode: HttpStatus.MOVED_PERMANENTLY,
+      };
+    } else {
+      return {
+        url: '/users/wallet',
+        statusCode: 302,
+      };
+    }
+  }
+  @Get('/account')
+  redirectAccount() {
+    return 'Account Working';
+  }
+  @Get('/wallet')
+  redirectWallet() {
+    return 'Wallet Working';
   }
 }
