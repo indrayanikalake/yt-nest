5a87a7f3e8f66231ea85c4acf7c768e35bafa9d8
Indrayani <indrayanikalake13@gmail.com>
2024-04-26 15:04:35 +0530

    Route Parameters | Query Parameters

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,11 +1,33 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, Param, Query, Req, Res } from '@nestjs/common';
 import { of } from 'rxjs';
 import { Request, Response } from 'express';
 
+interface ProfileParams {
+  id: number;
+  name: string;
+}
+
 @Controller('/users')
 export class UsersController {
-  @Get('/profile')
-  async getProfile(@Req() req: Request, @Res() res: Response) {
+  @Get('/profile/:id/:name')
+  async getProfile(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+    @Param() params: ProfileParams,
+    //Param('id') param: number,
+  ) {
+    console.log(params.name);
+    // return new Promise((resolve) => resolve({ hello: 'world' }));
+    return of({
+      hello: 'world',
+    });
+  }
+  @Get('/video')
+  getVideo(
+    @Query() query: ProfileParams,
+    //Headers('user-agent') headers: Record<string, any>,
+  ) {
+    console.log(query.name);
     // return new Promise((resolve) => resolve({ hello: 'world' }));
     return of({
       hello: 'world',
